15f277bd8a08a9e0c2f9358f3a441990449204b1
Alex Scott <alexanderscott@outlook.com>
2021-09-14 13:43:17 +0000

    Add initial bazel deps

---
+++ .devcontainer/library-scripts/bazel.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e 
+
+# Install Bazel if not already installed 
+if type bazel > /dev/null 2>&1; then
+    echo "Bazel already installed."
+else
+    BAZEL_VERSION=4.1.0
+    BAZEL_DOWNLOAD_SHA=dev-mode
+    curl -fSsL -o /tmp/bazel-installer.sh https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh \
+        && ([ "${BAZEL_DOWNLOAD_SHA}" = "dev-mode" ] || echo "${BAZEL_DOWNLOAD_SHA} */tmp/bazel-installer.sh" | sha256sum --check - ) \
+        && /bin/bash /tmp/bazel-installer.sh --base=/usr/local/bazel \
+        && rm /tmp/bazel-installer.sh
+fi
+
+# Install Buildifier if not already installed 
+if type buildifier > /dev/null 2>&1; then
+    echo "Buildifier already installed."
+else
+    BUILDIFIER_VERSION=4.2.0
+    curl -sSL "https://github.com/bazelbuild/buildtools/releases/download/${BUILDIFIER_VERSION}/buildozer-linux-amd64" -o /usr/local/bin/buildifier
+    chmod +x /usr/local/bin/buildifier
+fi
