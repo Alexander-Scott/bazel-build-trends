20965f50dac32cf9dd64c48cec61b139c0807a95
Alexander Scott <alexanderscott@outlook.com>
2021-10-05 19:01:52 +0200

    Fix linter

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -37,7 +37,7 @@ type server struct {
 	pb.UnimplementedGreeterServer
 }
 
-// SayHello implements helloworld.GreeterServer
+// SayHello implements helloworld.GreeterServer.
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
